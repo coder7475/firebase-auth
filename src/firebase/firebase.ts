6d08c1082bbe4c98dbdb32dd307fa94175c40f99
coder7475 <robiulhossain7475@gmail.com>
2024-01-06 19:35:11 +0600

    feat: initialize firebase app and export its auth

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirebaseConfig } from './firebase.config';
+
+// get firebase config
+const firebaseConfig = getFirebaseConfig();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+// export it
+export default auth;
