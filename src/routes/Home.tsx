3dddf43f5cb3bd9be644f5f0b06089e9e631a2e1
coder7475 <robiulhossain7475@gmail.com>
2024-01-11 18:05:35 +0600

    feat: add email and password field with a submit button in home UI

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
-import reactLogo from '../assets/react.svg'
+import reactLogo from "../assets/react.svg";
 
 const defaultFormFields = {
   email: "",
@@ -25,6 +25,29 @@ const Home = () => {
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
+        <form >
+          <div>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div>
+            <input id="recaptcha" type="submit" />
+          </div>
+        </form>
       </div>
     </div>
   );
